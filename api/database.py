753f720dfd6a7467513b445b3c2cf0d4aa411d4e
Alvaro Martin <almarlop98@gmail.com>
2022-06-19 19:40:41 +0200

    Added more exceptions and get_db_collection function

--- api/database.py
+++ api/database.py
@@ -1,6 +1,8 @@
 """Module for database connection"""
 from pymongo import MongoClient
 
+from exceptions import CollectionNotFound
+
 BUG_REPORTS_COLLECTION = "bug_reports"
 
 def connect_to_mongo_database(host: str, port: int, db_name: str):
@@ -15,3 +17,20 @@ def connect_to_mongo_database(host: str, port: int, db_name: str):
     client = MongoClient(host, port)
     db = client[db_name]
     return db
+
+def get_db_collection(db: MongoClient, collection_name: str):
+    """
+    Get the collection from the database.
+
+    Args:
+        db: database connection.
+        collection_name: name of the collection.
+    Returns:
+        collection: collection from the database.
+    Exceptions:
+        CollectionNotFound. If the collection does not exist.
+    """
+    if collection_name not in db.list_collection_names():
+        raise CollectionNotFound("collection_name")
+
+    return db[collection_name]
--- api/exceptions.py
+++ api/exceptions.py
@@ -1,7 +1,38 @@
 """Api exceptions list"""
 
+# General
 class ApiException(Exception):
     """Base class for Api exceptions"""
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+# Database
+class CollectionNotFound(ApiException):
+    """
+    Exception raised when a database collection is not found
+    
+    Args:
+        collection_name: name of the collection
+    """
+    def __init__(self, collection_name: str):
+        super().__init__(f"Collection '{collection_name}' not found")
+
+# Reports
+class NoReportsFound(ApiException):
+    """
+    Exception raised when no reports are found
+    """
+    def __init__(self):
+        super().__init__("No reports found")
+
+class ReportNotFound(ApiException):
+    """
+    Exception raised when a report is not found
+    
+    Args:
+        report_id: id of the report
+    """
+    def __init__(self, report_id: str):
+        super().__init__(f"Report '{report_id}' not found")
